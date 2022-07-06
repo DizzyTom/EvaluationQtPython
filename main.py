491c2c833d53778eb05df4807dbfa17f415ba4a0
DizzyTom <3038816978@qq.com>
2022-07-07 00:28:07 +0800

    fix icon

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from PySide6.QtWidgets import QApplication
 import PySide6
 import os
 from modules.main import MainWindow
-# Configure the environment of Qt
-dirname = os.path.dirname(PySide6.__file__)
-plugin_path = os.path.join(dirname, 'plugins', 'platforms')
-os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
-
 
 # Main Program
 if __name__ == '__main__':
